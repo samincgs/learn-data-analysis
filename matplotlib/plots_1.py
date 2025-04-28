4889e9a741364b1ee4b4d3130cd6bcf46bcae95c
Samin <samincgs@gmail.com>
2025-04-27 23:38:13 -0400

    started plotting the graph with sample data from stackoverflows 2018 developer earnings

--- matplotlib/plots_1.py
+++ matplotlib/plots_1.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 # DATA
 # x-axis values (age ranges)
-ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+median_salaries_ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 # y-axis values (median salaries)
-dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+median_salaries_devs_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 
 # python developers by age (no need since its repetitive)
 py_dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35] 
@@ -17,3 +17,7 @@ py_dev_y = [45372, 48876, 53850, 57287, 63016,
 js_dev_y = [37810, 43515, 46823, 49293, 53437,
             56373, 62375, 66674, 68745, 68746, 74583]
 
+
+plt.plot(median_salaries_ages_x, median_salaries_devs_y)
+
+plt.show()
